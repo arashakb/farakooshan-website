ab0d6cb8f9389bf53935936582b4ed2959865f8a
Arash <arashakbari1234@gmail.com>
2023-12-04 21:19:40 +0330

    updates

--- accounts/urls.py
+++ accounts/urls.py
@@ -4,6 +4,6 @@ from accounts.views import *
 app_name = 'accounts'
 
 urlpatterns = [
-    path('login/', login_view, name='login'),
-    path('logout', logout_view, name='logout'),
+    path('login', login_view, name="login"),
+    path('logout', logout_view, name="logout"),
 ]
