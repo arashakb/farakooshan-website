578a567e10f0ae8ee2a42bf88056e02d6c58d7f1
Arash <arashakbari1234@gmail.com>
2024-02-03 16:21:58 +0330

    dev_prod preparation

---
+++ farakooshan_website/setting/dev.py
@@ -0,0 +1,29 @@
+from farakooshan_website.settings import *
+
+# Quick-start development settings - unsuitable for production
+# See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
+
+# SECURITY WARNING: keep the secret key used in production secret!
+SECRET_KEY = 'django-insecure-nffof$ji3a6zor_*0_&q%c*swjwh-1*6pvi^4dd=s&3c#2pfp#'
+
+# SECURITY WARNING: don't run with debug turned on in production!
+DEBUG = True
+
+ALLOWED_HOSTS = []
+
+# Database
+# https://docs.djangoproject.com/en/3.2/ref/settings/#databases
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
+    }
+}
+
+STATIC_ROOT = BASE_DIR / 'static'
+MEDIA_ROOT = BASE_DIR / 'media'
+
+STATICFILES_DIRS = [
+    BASE_DIR / 'statics',
+]
