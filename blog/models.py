9ffa57f57799a2a24773f6107c5cef5960efa3c7
Arash <arashakbari1234@gmail.com>
2024-02-07 11:25:20 +0330

    bug fixed

--- blog/models.py
+++ blog/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 from django.contrib.auth.models import User
+from ckeditor.fields import RichTextField
 # Create your models here.
 class Category(models.Model):
     name = models.CharField(max_length=255)
@@ -10,7 +11,7 @@ class Post(models.Model):
     image = models.ImageField(upload_to='blog/', default='blog/default.jpg')
     author = models.ForeignKey(User, on_delete=models.SET_NULL, null=True)
     title = models.CharField(max_length=255)
-    content = models.TextField()
+    content = RichTextField(blank=True, null=True)
     # tag 
     category = models.ManyToManyField(Category)
     # course
