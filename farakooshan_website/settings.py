d9a516d5d94cf5beefdac2fde5bd8e110787c489
Arman Akbari <armanakbari456@gmail.com>
2024-02-07 23:09:57 +0330

    قل

--- farakooshan_website/settings.py
+++ farakooshan_website/settings.py
@@ -12,7 +12,7 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 
 from pathlib import Path
 from django.conf import global_settings
-
+import os
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -124,3 +124,6 @@ MEDIA_URL = '/media/'
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
+LOCALE_PATHS = (
+    os.path.join(BASE_DIR, 'locale'),
+)
